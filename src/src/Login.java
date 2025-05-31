94f7afdedd2bdda24b5510d8e7377a5f0b1f90d1
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-05-31 15:43:19 -0400

    completed login frame. started signup frame

--- src/src/Login.java
+++ src/src/Login.java
@@ -10,22 +10,82 @@ public class Login extends JFrame {
     public Login(){
         setSize(900, 400);
         setLocation(350, 275);
-        setVisible(true);
         setLayout(null);
+        getContentPane().setBackground(Color.WHITE);
 
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(131, 193, 233));
         p1.setBounds(0, 0, 400, 400);
         p1.setLayout(null);
         add(p1);
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.png"));
-        System.out.println(i1.getImageLoadStatus());
         Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel login_image = new JLabel(i3);
         login_image.setBounds(100, 120, 200, 200);
         p1.add(login_image);
 
+        JPanel p2 = new JPanel();
+        p2.setLayout(null);
+        p2.setBounds(400, 30, 450, 300);
+        add(p2);
+
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(60, 20, 100, 25);
+        lblusername.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        p2.add(lblusername);
+
+        JTextField tfusername = new JTextField();
+        tfusername.setBounds(60, 60, 300, 30);
+        tfusername.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tfusername);
+
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(60, 105, 100, 25);
+        lblpassword.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        p2.add(lblpassword);
+
+        JTextField tfpassword = new JTextField();
+        tfpassword.setBounds(60, 140, 300, 30);
+        tfpassword.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tfpassword);
+
+        JButton login = new JButton("Login");
+        login.setBounds(60, 200, 130, 30);
+        login.setBackground(new Color(131, 193, 233));
+        login.setForeground(Color.WHITE);
+        login.setBorder(BorderFactory.createEmptyBorder());
+        login.setOpaque(true);
+        login.setBorderPainted(false);
+        p2.add(login);
+
+        JButton signup = new JButton("Sign Up");
+        signup.setBounds(230, 200, 130, 30);
+        signup.setBackground(new Color(131, 193, 233));
+        signup.setForeground(Color.WHITE);
+        signup.setBorder(BorderFactory.createEmptyBorder());
+        signup.setOpaque(true);
+        signup.setBorderPainted(false);
+        p2.add(signup);
+
+        JButton forgot_password = new JButton("Forgot Password");
+        forgot_password.setBounds(140, 240, 130, 30);
+        forgot_password.setBackground(Color.WHITE);
+        forgot_password.setForeground(Color.BLACK);
+        forgot_password.setBorder(BorderFactory.createEmptyBorder());
+        forgot_password.setOpaque(true);
+        forgot_password.setBorderPainted(false);
+        p2.add(forgot_password);
+
+        JLabel text = new JLabel("Trouble Signing in?");
+        text.setBounds(280, 245, 150, 20);
+        text.setForeground(Color.RED);
+        p2.add(text);
+
+
+
+        setVisible(true);
 
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/src/Signup.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * A class handling the signup of a new user to the travel management system
+ */
+
+public class Signup extends JFrame {
+
+    public Signup(){
+        setBounds(350, 200, 900, 400);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(131, 193, 233));
+        p1.setBounds(0, 0, 500, 400);
+        p1.setLayout(null);
+        add(p1);
+
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setFont(new Font("Tahoma", Font.BOLD, 18));
+        lblusername.setBounds(50, 20, 125, 25);
+        p1.add(lblusername);
+
+        JTextField tfusername = new JTextField();
+        tfusername.setBounds(190, 20, 180, 25);
+        tfusername.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tfusername);
+
+        JLabel name = new JLabel("Name");
+        name.setFont(new Font("Tahoma", Font.BOLD, 18));
+        name.setBounds(50, 60, 125, 25);
+        p1.add(name);
+
+        JTextField tfname = new JTextField();
+        tfname.setBounds(190, 60, 180, 25);
+        tfname.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tfname);
+
+        JLabel password = new JLabel("Password");
+        password.setFont(new Font("Tahoma", Font.BOLD, 18));
+        password.setBounds(50, 100, 125, 25);
+        p1.add(password);
+
+        JTextField tfpassword = new JTextField();
+        tfpassword.setBounds(190, 100, 180, 25);
+        tfpassword.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tfpassword);
+
+        JLabel lblsecurity = new JLabel("Security Ques");
+        lblsecurity.setFont(new Font("Tahoma", Font.BOLD, 18));
+        lblsecurity.setBounds(50, 140, 125, 25);
+        p1.add(lblsecurity);
+
+        Choice security = new Choice();
+        security.add("Fav series/movie character");
+        security.add("Fav series/movies");
+        security.add("Your lucky number");
+        security.add("Childhood nickname");
+        security.setBounds(190, 140, 190, 25);
+        security.setBackground(Color.WHITE);
+        p1.add(security);
+
+        JLabel sec_ans = new JLabel("Security Answer");
+        sec_ans.setFont(new Font("Tahoma", Font.BOLD, 18));
+        sec_ans.setBounds(50, 180, 125, 25);
+        p1.add(sec_ans);
+
+        JTextField tfsec_ans = new JTextField();
+        tfsec_ans.setBounds(190, 180, 180, 25);
+        tfsec_ans.setBorder(BorderFactory.createEmptyBorder());
+        p1.add(tfsec_ans);
+
+        JButton create = new JButton("Create User");
+        create.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
+        create.setBackground(Color.WHITE);
+        create.setForeground(Color.BLACK);
+        create.setBounds(50, 250, 130, 30);
+        create.setOpaque(true);
+        create.setBorder(BorderFactory.createEmptyBorder());
+        create.setBorderPainted(false);
+        p1.add(create);
+
+        JButton back = new JButton("Back");
+        back.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
+        back.setBackground(Color.WHITE);
+        back.setForeground(Color.BLACK);
+        back.setBounds(240, 250, 130, 30);
+        back.setOpaque(true);
+        back.setBorder(BorderFactory.createEmptyBorder());
+        back.setBorderPainted(false);
+        p1.add(back);
+
+
+
+
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args){
+        new Signup();
+    }
+}
